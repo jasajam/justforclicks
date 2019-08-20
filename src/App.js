d6133094a898d442e2c1a152dbc6cf1991ef04f7
JAM <27083244+jasajam@users.noreply.github.com>
2019-08-20 00:43:56 -0700

    added necessary files from creating React app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// all our components need to be in app
+  // Header (scores to be passed here), Jumbotron, TileSection (holds all the data we pass to it), Footer
+// stateful functionality needs to be here
+  // which tile gets clicked on [{ id: 1, photo: 'url', description: '', clicked: false }, ...]
+  // currentScore - how many user has in a row so far
+  // highestScore - only changes when it surpasses currentScore
+// functions we'll need
+  // clickHandler as you click on a tile -> need to pass the id to this fn to know which object to change clicked value to true/call end of game, then check if correct/incorrect guess
+  // fns for correct/incorrect guess
+  // shuffle fn
+  // reset fn to be called when the game ends
+
+
+class App extends Component {
+  state = {
+    tileData: [{}],
+    currentScore: 0,
+    highestScore: 0
+  }
+
+  render() {
+    return (
+      <div className="App">
+        Jasa
+      </div>
+    );
+  }
 }
 
 export default App;
